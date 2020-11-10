3fa1e83292a46953ba8cf5cf0362b3506026c751
killjer215 <gerardo.prado01@utrgv.edu>
2020-11-10 02:57:41 -0600

    Update calc.c

--- calc.c
+++ calc.c
@@ -75,6 +75,7 @@ void *adder(void *arg)
     int startOffset, remainderOffset;
     int i, sum, operlength;
 	char *operand;
+	char *temp[20];
 	
      /* remove this line */
 	
@@ -120,7 +121,7 @@ void *adder(void *arg)
 		       sum = value1 + value2;
 
 		       
-		       sprint(operand, "%d", sum);
+		       operand = int2string(sum, temp);
 
 		       operlength = strlen(operand);
 		       
@@ -159,6 +160,7 @@ void *multiplier(void *arg)
     int startOffset, remainderOffset;
     int i, sum, operlength;
 	char *operand;
+	char *temp;
 	
 
     return NULL; /* remove this line */
@@ -204,7 +206,7 @@ void *multiplier(void *arg)
 		       sum = value1 * value2;
 
 		       
-		       sprint(operand, "%d", sum);
+		       operand = int2string(sum, temp);
 
 		       operlength = strlen(operand);
 		       
